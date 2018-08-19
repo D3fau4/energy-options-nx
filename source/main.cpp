78825e3f29f38c5a74649eb08e19f3d3d91e8fd3
D3fau4 <raulwii2@gmail.com>
2018-08-19 12:53:10 +0200

    add reboot

---
+++ source/main.cpp
@@ -0,0 +1,20 @@
+#include <string.h>
+#include <stdio.h>
+
+#include <switch.h>
+
+extern "C"
+{
+	#include "bpc.h"
+}
+
+
+int main(int argc, char **argv)
+{
+    gfxInitDefault();
+    consoleInit(NULL);
+    bpcInitialize();
+    bpcRebootSystem();
+    return 0;
+}
+
